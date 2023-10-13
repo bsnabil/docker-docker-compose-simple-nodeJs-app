bcd82fed4a3357af94d54e2c3a7a4edf72862698
bsnabil <125509526+bsnabil@users.noreply.github.com>
2023-10-13 01:04:24 -1100

    es
    es

---
+++ dockerfile
@@ -0,0 +1,12 @@
+FROM node:18 as base_stage
+FROM base_stage as development
+WORKDIR /app
+COPY package.json  .
+#NODEJS DEP
+RUN npm install
+#RUN npm install express twilio
+#RUN npm install -g nodemon
+COPY . .
+EXPOSE 4000
+CMD [ "npm","run","start-dev" ]
+FROM base_stage as production
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const port = 4000;
+
+ 
+
+app.get('/', (req, res) => {
+  res.send('<h3> [+] step nodemone passed [+] </h3>');
+});
+app.get('/', (req, res) => {
+  res.send('<h3> [+] step mongodb is loadding to connect ... [+] </h3>');
+});
+ 
