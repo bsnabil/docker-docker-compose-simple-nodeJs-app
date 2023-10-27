9b7e6e911316b2aa2778c41b14f1d8a839c321bb
Ben salem Nabil <125509526+bsnabil@users.noreply.github.com>
2023-10-26 22:29:39 -1100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 const port = 4000;
